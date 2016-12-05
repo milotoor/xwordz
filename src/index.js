7013808ddb0d8cd51e2ac2de0961172f93fd9392
Milo Toor <mtoor@omicia.com>
2016-12-04 21:36:59 -0800

    Update reducer import

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import reducer from './app/reducer';
+import reducer from './app/reducers';
 import { Main } from './app/main';
 
 import '../vendor/material.light_blue-green.min.css';
