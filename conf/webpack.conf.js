d2bcffb4b5a51638003c3d1c2723c6469e685ec0
Milo Toor <mtoor@omicia.com>
2017-01-11 21:47:08 -0800

    Include Babel polyfill in tests, development

--- conf/webpack.conf.js
+++ conf/webpack.conf.js
@@ -65,6 +65,7 @@ module.exports = {
     entry: [
         'webpack/hot/dev-server',
         'webpack-hot-middleware/client',
+        'babel-polyfill',
         `./${conf.path.src('index')}`
     ]
 };
--- test/test_helper.js
+++ test/test_helper.js
@@ -3,5 +3,10 @@ import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 import dirtyChai from 'dirty-chai';
 
+// Augments the prototypes of base types to include ES6 methods, and more
+if (!window._babelPolyfill) {
+    require('babel-polyfill');
+}
+
 chai.use(dirtyChai);
 chai.use(chaiImmutable);
